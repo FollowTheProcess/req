e568b18abff68e023900e8ed7ddfb6165fbf6ec8
Tom Fleet <tomfleet2018@gmail.com>
2025-03-08 18:30:39 +0000

    Working state machine based scanner

--- internal/syntax/scanner/scanner.go
+++ internal/syntax/scanner/scanner.go
@@ -4,7 +4,6 @@ package scanner
 import (
 	"fmt"
 	"io"
-	"unicode"
 	"unicode/utf8"
 
 	"github.com/FollowTheProcess/req/internal/syntax/token"
@@ -12,8 +11,10 @@ import (
 
 const (
 	eof = rune(-1) // eof signifies we have reached the end of the input
-	bom = 0xFEFF   // byte order mark, only permitted as very first character
 )
+
+// scanFn represents the state of the scanner as a function that returns the next state.
+type scanFn func(*Scanner) scanFn
 
 // An ErrorHandler may be provided to the [Scanner]. If a syntax error is encountered and
 // a non-nil handler was provided, it is called with the position info and error message.
@@ -21,14 +22,15 @@ type ErrorHandler func(pos Position, msg string)
 
 // Scanner is the http file scanner.
 type Scanner struct {
-	handler   ErrorHandler // The error handler, if any
-	name      string       // Name of the file
-	src       []byte       // Raw source text
-	start     int          // The start position of the current token
-	pos       int          // Current scanner position in src (bytes, 0 indexed)
-	line      int          // Current line number (1 indexed)
-	lineStart int          // Offset at which the current line began
-	char      rune         // The character the scanner is currently sat on
+	handler   ErrorHandler     // The error handler, if any
+	tokens    chan token.Token // Channel on which to emit scanned tokens
+	name      string           // Name of the file
+	src       []byte           // Raw source text
+	start     int              // The start position of the current token
+	pos       int              // Current scanner position in src (bytes, 0 indexed)
+	line      int              // Current line number (1 indexed)
+	lineStart int              // Offset at which the current line started
+	width     int              // Width of the last rune read from input, so we can backup
 }
 
 // New returns a new [Scanner] that reads from r.
@@ -39,88 +41,108 @@ func New(name string, r io.Reader, handler ErrorHandler) (*Scanner, error) {
 		return nil, fmt.Errorf("could not read from input: %w", err)
 	}
 
+	// TODO(@FollowTheProcess): Benchmark to find the right buffer size for the channel
+
 	s := &Scanner{
+		handler: handler,
+		tokens:  make(chan token.Token),
 		name:    name,
 		src:     src,
+		start:   0,
+		pos:     0,
 		line:    1,
-		handler: handler,
-		char:    ' ',
+		width:   0,
 	}
 
-	// Read the first char, and ignore it if it's the bom
-	s.advance()
-	if s.char == bom {
-		s.advance()
-		s.start = s.pos
-	}
-
+	go s.run()
 	return s, nil
 }
 
 // Scan scans the input and returns the next token.
 func (s *Scanner) Scan() token.Token {
-	char := s.char // The current char before advancing
-	s.advance()
-
-	switch char {
-	case eof:
-		return s.token(token.EOF)
-	case '#':
-		if s.char == '#' {
-			// It's a request separator
-			return s.scanRequestSeparator()
-		}
-		return s.scanComment()
-	case '/':
-		return s.scanComment()
-	default:
-		if unicode.IsLetter(char) {
-			return s.scanText()
-		}
-		s.errorf("unexpected char %q", char)
-		return s.token(token.Error)
-	}
+	return <-s.tokens
 }
 
-// advance advances the scanner by a single character.
-func (s *Scanner) advance() {
+// advance returns, and consumes, the next character in the input or [eof].
+func (s *Scanner) advance() rune {
 	if s.pos >= len(s.src) {
-		s.char = eof
 		s.pos = len(s.src)
-		return
+		return eof
 	}
 
 	char, width := utf8.DecodeRune(s.src[s.pos:])
 	if char == utf8.RuneError {
 		s.errorf("invalid utf8 char: %U", char)
+		// Advance to the end to prevent cascade errors
+		s.pos = len(s.src)
+		return eof
 	}
 
+	s.width = width
+	s.pos += width
 	if char == '\n' {
 		s.line++
-		s.lineStart = s.pos
 	}
 
-	// Move the scanner forward
-	s.pos += width
-	s.char = char
+	return char
 }
 
-// advanceWhile advances the scanner as long as the predicate returns true, stopping at
-// the first character for which it returns false.
-func (s *Scanner) advanceWhile(predicate func(r rune) bool) {
-	for predicate(s.char) {
-		s.advance()
+// peek returns, but does not consume, the next character in the input or [eof].
+func (s *Scanner) peek() rune {
+	if s.pos >= len(s.src) {
+		return eof
 	}
+
+	char, _ := utf8.DecodeRune(s.src[s.pos:])
+	if char == utf8.RuneError {
+		s.errorf("invalid utf8 char: %U", char)
+		// Advance to the end to prevent cascade errors
+		s.pos = len(s.src)
+		return eof
+	}
+
+	return char
 }
 
-// token returns a token of a particular kind, using the scanner state
-// to fill in position info.
-func (s *Scanner) token(kind token.Kind) token.Token {
-	tok := token.Token{Kind: kind, Start: s.start, End: s.pos}
+// char returns the character the scanner is currently sat on or [eof].
+func (s *Scanner) char() rune {
+	if s.pos >= len(s.src) {
+		return eof
+	}
+	return rune(s.src[s.pos])
+}
 
-	// Bring start up to pos
+// emit passes a token over the tokens channel, using the scanner's internal
+// state to populate position information.
+func (s *Scanner) emit(kind token.Kind) {
+	s.tokens <- token.Token{
+		Kind:  kind,
+		Start: s.start,
+		End:   s.pos,
+	}
 	s.start = s.pos
-	return tok
+}
+
+// run starts the state machine for the scanner, it runs with each [scanFn] returning the next
+// state until one returns nil (typically an error or eof), at which point the tokens channel
+// is closed as a signal to the receiver that no more tokens will be sent.
+func (s *Scanner) run() {
+	for state := scanStart; state != nil; {
+		state = state(s)
+	}
+	close(s.tokens)
+}
+
+// scanStart is the initial state of the scanner.
+func scanStart(s *Scanner) scanFn {
+	switch s.advance() {
+	case eof:
+		s.emit(token.EOF)
+		return nil // Break the state machine
+	default:
+		s.errorf("unexpected token %q", string(s.char()))
+		return nil
+	}
 }
 
 // error calls the installed error handler using information from the state
@@ -149,66 +171,4 @@ func (s *Scanner) error(msg string) {
 // errorf calls error with a formatted message.
 func (s *Scanner) errorf(format string, a ...any) {
 	s.error(fmt.Sprintf(format, a...))
-}
-
-// scanText scans a string of continuous characters, stopping at the first
-// whitespace character.
-func (s *Scanner) scanText() token.Token {
-	for !unicode.IsSpace(s.char) && s.char != eof {
-		s.advance()
-	}
-
-	text := string(s.src[s.start:s.pos])
-	kind, method := token.Method(text)
-	if method {
-		return s.token(kind)
-	}
-
-	return s.token(token.Text)
-}
-
-// scanComment scans either a '#' or '//' style comment.
-//
-// The '#' or first '/' has been consumed.
-func (s *Scanner) scanComment() token.Token {
-	s.advanceWhile(isLineSpace)
-
-	// Consume until the end of the line or eof
-	for s.char != '\n' && s.char != eof {
-		s.advance()
-	}
-
-	tok := s.token(token.Comment)
-
-	// Any trailing space after the comment doesn't matter
-	s.advanceWhile(unicode.IsSpace)
-
-	return tok
-}
-
-// scanRequestSeparator scans the literal '###' separator.
-//
-// The first '#' has already been consumed.
-func (s *Scanner) scanRequestSeparator() token.Token {
-	for s.char == '#' {
-		s.advance()
-	}
-
-	text := string(s.src[s.start:s.pos])
-	if text != "###" {
-		s.errorf("bad request separator, expected %q got %q", "###", text)
-		return s.token(token.Error)
-	}
-
-	tok := s.token(token.RequestSeparator)
-
-	s.advanceWhile(unicode.IsSpace)
-
-	return tok
-}
-
-// isLineSpace reports whether r is a non line terminating whitespace. Imagine
-// [unicode.IsSpace] but without '\n' and '\r\n'.
-func isLineSpace(r rune) bool {
-	return r == ' ' || r == '\t'
 }
