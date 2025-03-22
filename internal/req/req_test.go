2247f455236b53a74ecc7a593147f8d984ed1e77
Tom Fleet <tomfleet2018@gmail.com>
2025-03-22 12:06:01 +0000

    Scrap that idea

--- internal/req/req_test.go
+++ internal/req/req_test.go
@@ -2,7 +2,6 @@ package req_test
 
 import (
 	"bytes"
-	"flag"
 	"fmt"
 	"path/filepath"
 	"runtime"
@@ -10,13 +9,7 @@ import (
 	"testing"
 
 	"github.com/FollowTheProcess/req/internal/req"
-	"github.com/FollowTheProcess/snapshot"
 	"github.com/FollowTheProcess/test"
-)
-
-var (
-	update = flag.Bool("update", false, "Update snapshots")
-	clean  = flag.Bool("clean", false, "Clean all snapshots and recreate")
 )
 
 func TestCheck(t *testing.T) {
@@ -68,47 +61,4 @@ func TestCheck(t *testing.T) {
 		// Stdout should be empty
 		test.Equal(t, stdout.String(), "")
 	})
-}
-
-func TestShow(t *testing.T) {
-	tests := []struct {
-		name    string          // Name of the test case
-		options req.ShowOptions // Options to pass simulating CLI flags
-	}{
-		{
-			name:    "default",
-			options: req.ShowOptions{},
-		},
-		{
-			name:    "json",
-			options: req.ShowOptions{JSON: true},
-		},
-		{
-			name:    "resolved",
-			options: req.ShowOptions{Resolve: true},
-		},
-		{
-			name:    "resolved json",
-			options: req.ShowOptions{Resolve: true, JSON: true},
-		},
-	}
-
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			snap := snapshot.New(t, snapshot.Clean(*clean), snapshot.Update(*update))
-
-			file := filepath.Join("testdata", "show", "full.http")
-
-			stdout := &bytes.Buffer{}
-			stderr := &bytes.Buffer{}
-
-			r := req.New(stdout, stderr)
-
-			err := r.Show(file, tt.options)
-			test.Ok(t, err)
-
-			test.Equal(t, stderr.String(), "")
-			snap.Snap(stdout.String())
-		})
-	}
 }
