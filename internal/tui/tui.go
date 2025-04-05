88c770d7f5c1ee0304fc19a36958b5f61e92d94b
Tom Fleet <tomfleet2018@gmail.com>
2025-04-05 13:17:50 +0100

    Add some notes

--- internal/tui/tui.go
+++ internal/tui/tui.go
@@ -17,6 +17,10 @@ const errorClearAfter = 2 * time.Second
 
 // TODO(@FollowTheProcess): I want to understand all this a bit more, atm it's basically copy pasted from the bubbles filepicker example
 // with a bit of bodgery to show the help. Perhaps I need to make my own bubbles to do all this, then I'll understand it a lot more
+// would also let me play with some ideas like:
+// - Reading the file and showing in a preview window on hover (files only)
+// - Once selected a file, parse it and then have a fancy list bubble of the http request the cursor is on
+// - On enter, it's basically now just `req do <file> <request>` so close the TUI and do the request
 
 // Run runs the TUI, this is what happens when users call `req` with no arguments.
 func Run() error {
